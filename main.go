4813696bffafa7cb621bf8126e10014bf45f9441
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-24 22:39:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -21,3 +21,32 @@ func main()  {
 	http.ListenAndServe(":4000",sm)
 }
 
+        //s := &http.Server{
+	//	Addr: ":4000",
+	//	Handler: sm,
+	//	IdleTimeout: 120*time.Second,
+	//	ReadTimeout: 1*time.Second,
+	//	WriteTimeout: 1*time.Second,
+	//}
+	//
+	//go func() {
+	//	err := s.ListenAndServe()
+	//
+	//	if err != nil {
+	//		l.Fatal(err)
+	//	}
+	//
+	//}()
+	//
+	//sigchan := make(chan os.Signal)
+	//signal.Notify(sigchan,os.Interrupt)
+	//signal.Notify(sigchan,os.Kill)
+	//
+	//sig := <- sigchan
+	//
+	//l.Println("recieved terminate...",sig)
+	//
+	//tc, _ := context.WithTimeout(context.Background(),30*time.Second)
+	//
+	//s.Shutdown(tc)
+
